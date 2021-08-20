460fdba5edaa33697bad4ff12780ca2dc848f3f0
zhenhai.zhao <123456>
2021-08-20 11:26:37 +0800

    建立视频拍摄类

--- app/src/main/java/com/menghai/camerademo/MainActivity.kt
+++ app/src/main/java/com/menghai/camerademo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.menghai.camerademo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.menghai.camerademo.mediacodec.MediaCodecActivity
 
+/**
+ * 这是
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         findViewById<Button>(R.id.mediaCodecB).setOnClickListener {
+             startActivity(Intent(this, MediaCodecActivity::class.java))
+         }
     }
 }
---
+++ app/src/main/java/com/menghai/camerademo/mediacodec/MediaCodecActivity.kt
@@ -0,0 +1,22 @@
+package com.menghai.camerademo.mediacodec
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.menghai.camerademo.R
+
+/**
+ * @author      zhenhai.zhao
+ * @date        2021/8/20 11:18 AM
+ * @package     com.menghai.camerademo.mediacodec
+ * @email       zhenhai.zhao@asiainnovations.com
+ * @description MediaCodec视频拍摄类
+ **/
+class MediaCodecActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_codec)
+
+    }
+}
