18b63db192dc0288a85e42f5750538134de1d615
zhenhai.zhao <123456>
2021-08-23 10:27:34 +0800

    建立MediaCodec

--- app/src/main/java/com/menghai/camerademo/mediacodec/MediaCodecActivity.kt
+++ app/src/main/java/com/menghai/camerademo/mediacodec/MediaCodecActivity.kt
@@ -1,7 +1,9 @@
 package com.menghai.camerademo.mediacodec
 
+import android.media.MediaCodec
+import android.media.MediaCodecList
+import android.media.MediaFormat
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.menghai.camerademo.R
 
@@ -17,6 +19,15 @@ class MediaCodecActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_codec)
+       // MediaCodec 编码接口
+        val mediaCodec = MediaCodec.createEncoderByType("video")
+
+        val videoFormat = MediaFormat.createVideoFormat("video/avc", 640, 480)
+        videoFormat.setInteger(MediaFormat.KEY_BIT_RATE, 600) //比特率
+        videoFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 30) //帧率
+        videoFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 10) //指定关键帧时间间隔
+
+        mediaCodec.configure(videoFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
 
     }
 }
